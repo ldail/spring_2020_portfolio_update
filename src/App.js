94ac5b4a8491ca26c5802192dfcab0503c94d505
ldail <leondailofficial@gmail.com>
2020-04-24 19:00:52 -0700

    part static data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import './App.css';
+import InitialSplash from './components/InitialSplash';
 
 function App() {
   return (
     <div className="App">
+
+      <header className="header">
+        <div className="nameHeader">{`{leon dail}`}</div>
+        <div className="menuHeader">...</div>
+      </header>
+
+      <InitialSplash />
+
     </div>
   );
 }
